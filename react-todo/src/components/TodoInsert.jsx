3fffadbb97edb632b5307a427dbc63565d9028ea
baeksunghyun <ces0135@naver.com>
2025-01-07 16:14:57 +0900

    기본 틀 구현

---
+++ react-todo/src/components/TodoInsert.jsx
@@ -0,0 +1,12 @@
+function TodoInsert() {
+    return ( 
+        <form>
+            <input type="text" placeholder="할일을 입력하세요."/>
+            <button type="submit">입력</button>
+        </form>
+
+     );
+}
+
+// 생성한 컴포넌트 내보내기
+export default TodoInsert;
---
+++ react-todo/src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+function TodoList() {
+    return ( 
+        <>
+            <ul>
+                <li>아이템 1</li>
+                <li>아이템 2</li>
+                <li>아이템 3</li>
+            </ul>
+        </>
+     );
+}
+
+// 생성한 컴포넌트 내보내기
+export default TodoList;
